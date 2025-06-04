123027a888fa1cbad025698b4b0427b9ab2bc7bf
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-06-04 16:24:30 +0100

    Ajouter CORS

--- src/main/java/tn/esprit/syshr/config/SecurityConfig.java
+++ src/main/java/tn/esprit/syshr/config/SecurityConfig.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,11 +30,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf-> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/absence/**").permitAll()
-                        .requestMatchers("/employee/**").permitAll()
+                        .requestMatchers("/employee/**").hasRole("EMPLOYEE")
                         .requestMatchers("/retard/**").permitAll()
-                        .requestMatchers("/admin/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/conge/**").permitAll()
                         .requestMatchers("/autorisation/**").permitAll()
                         .requestMatchers("/evaluation/**").permitAll()
@@ -40,5 +46,22 @@ public class SecurityConfig {
                         .authenticationProvider(authenticationProvider)
                         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Add Flutter web origin (http://localhost) and Angular origin (http://localhost:4200)
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",      // Angular
+                "http://localhost:4201"    // Previous Flutter port
+        ));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); // If you use cookies or auth headers
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
